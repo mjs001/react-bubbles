7f6e2c259af0b7b9a191bbf86f73b7d021414f43
miriam-sullivan <maljayd42@gmail.com>
2020-04-24 11:17:51 -0500

    got done with get and put

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -3,17 +3,19 @@ import axios from "axios";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
-
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
   useEffect(() => {
-    axios.get("/api/colors").then((res) => {
-      console.log("res", res);
-      ColorList(res);
-    });
-  });
+    axiosWithAuth()
+      .get("/api/colors")
+      .then((res) => {
+        console.log("res", res);
+        setColorList(res.data);
+      });
+  }, []);
 
   return (
     <>
--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import axios from "axios";
-
+import { axiosWithAuth } from "../utils/axiosWithAuth";
+import { useParams, useHistory } from "react-router-dom";
 const initialColor = {
   color: "",
-  code: { hex: "" }
+  code: { hex: "" },
 };
 
 const ColorList = ({ colors, updateColors }) => {
@@ -11,19 +12,28 @@ const ColorList = ({ colors, updateColors }) => {
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
 
-  const editColor = color => {
+  const editColor = (color) => {
     setEditing(true);
     setColorToEdit(color);
   };
 
-  const saveEdit = e => {
+  const saveEdit = (e) => {
     e.preventDefault();
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    axiosWithAuth()
+      .put(`/api/colors/${colorToEdit.id}`, colorToEdit)
+      .then((res) => {
+        updateColors(
+          colors.map((color) => (res.data.id === color.id ? res.data : color))
+        );
+        setEditing(false);
+      })
+      .catch((err) => console.log("error on save color", err));
   };
 
-  const deleteColor = color => {
+  const deleteColor = (color) => {
     // make a delete request to delete this color
   };
 
@@ -31,15 +41,17 @@ const ColorList = ({ colors, updateColors }) => {
     <div className="colors-wrap">
       <p>colors</p>
       <ul>
-        {colors.map(color => (
+        {colors.map((color) => (
           <li key={color.color} onClick={() => editColor(color)}>
             <span>
-              <span className="delete" onClick={e => {
-                    e.stopPropagation();
-                    deleteColor(color)
-                  }
-                }>
-                  x
+              <span
+                className="delete"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  deleteColor(color);
+                }}
+              >
+                x
               </span>{" "}
               {color.color}
             </span>
@@ -56,7 +68,7 @@ const ColorList = ({ colors, updateColors }) => {
           <label>
             color name:
             <input
-              onChange={e =>
+              onChange={(e) =>
                 setColorToEdit({ ...colorToEdit, color: e.target.value })
               }
               value={colorToEdit.color}
@@ -65,10 +77,10 @@ const ColorList = ({ colors, updateColors }) => {
           <label>
             hex code:
             <input
-              onChange={e =>
+              onChange={(e) =>
                 setColorToEdit({
                   ...colorToEdit,
-                  code: { hex: e.target.value }
+                  code: { hex: e.target.value },
                 })
               }
               value={colorToEdit.code.hex}
